4124875a034ab357101a55d33b6209544c879685
github Ko-Eunseo <github koyk0408@naver.com>
2021-09-27 14:59:41 +0900

    아큐웨더 clone

--- html5_css3/accuWeather/js/responsive.js
+++ html5_css3/accuWeather/js/responsive.js
@@ -35,7 +35,7 @@ $(function(){
         }
     });
 
-
+// 하위메뉴 안보이게 하기
     $(".lnb_container").on({
         "mouseleave" : function() {
             if (gnbFlag) {
@@ -49,5 +49,68 @@ $(function(){
         }
 
     });
+
+// 모바일 햄버거 메뉴 클릭시
+
+    $(".trigger").on({
+        "click" : function() {
+            // 햄버거 메뉴 버튼이 보일때 (햄버거 메뉴가 visible 상태인가?? 로 판단)
+            if($(".mobile.hamburger").is(":visible")) {
+                // 모바일 닫기 버튼 show
+                $(".mobile.hamburger").hide();
+                $(".mobile.close").show();
+
+                // 모바일 전체 메뉴 show
+                $("#main_header").height(60);
+                $("#main_content").hide();
+                $("#main_footer").hide();
+
+                $("#mobile_menu").empty();//#mobile_menu 하위 태그 초기화
+                $("li").eq(3).show();//mobile에서만 보이는 메뉴 2개 추가
+                $("li").eq(4).show();
+
+                var nav = $(".nav").clone();
+                $("#mobile_menu").append(nav);
+                $("#mobile_menu").show();
+            } else {
+                // 닫기 메뉴 버튼이 보일때
+                // 모바일 햄버거 버튼 show
+                $(".mobile.hamburger").show();
+                $(".mobile.close").hide();
+
+                // 모바일 전체 메뉴 show
+                $("#main_header").height(390);
+                $("#main_content").show();
+                $("#main_footer").show();
+                $("#mobile_menu").hide();
+            }
+
+        }
+    });
+
+    $(window).resize(function() {
+        var width = $(window).width();
+
+        if(width > 767) {
+            $("li").eq(3).hide();
+            $("li").eq(4).hide();
+
+            if($("mobile_menu").is(":visible")) {
+                $(".mobile.hamburger").show();
+                $(".mobile.close").hide();
+
+                $(".mobile.header").height(390);
+                $("#main_content").show();
+                $("#main_footer").show();
+
+                $("#mobile_menu").hide();
+            }
+        }
+
+
+    });
+
+
+
 
 });
