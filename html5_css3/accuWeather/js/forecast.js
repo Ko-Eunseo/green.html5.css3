5a5fca202695c2b6c002d44b5c90c61ec10edd82
github Ko-Eunseo <github koyk0408@naver.com>
2021-10-06 15:45:21 +0900

    weather binding

--- html5_css3/accuWeather/js/forecast.js
+++ html5_css3/accuWeather/js/forecast.js
@@ -35,5 +35,27 @@ function getCurrentWeather(city){
 
 // 현재날씨 온도 얻어오기
 function getCurrentTemp(city){
+    var temp;
+    var openWeatherAPI = url; //city가 계쏙 붙으므로 url초기화를 위해 반드시 넣어줌
+
+    $.ajax({
+        type: "GET",
+        url: openWeatherAPI += city, 
+        dataType: "json",
+        async: false, //결과 데이터를 리턴시키기 위해 동기 방식으로 변경
+        success: function(data) {
+            //정상 응답시 처리 작업: 바인딩
+            console.log(data);
+            temp = Math.floor(data.main.temp);//소수점 버림
+
+        },
+        error: function(request, status, error) {
+            //응답 에러시 처리 작업
+            console.log("code:" + request.staus);
+            console.log("message:" + request.responseText);
+            console.log("error" + error);
+        }
+    });
+    return temp;
 
 };
--- html5_css3/accuWeather/js/main.js
+++ html5_css3/accuWeather/js/main.js
@@ -1,2 +1,19 @@
-var result = getCurrentWeather("incheon");
-console.log(result);
+// var result = getCurrentWeather("incheon");
+// console.log(result);
+
+var cityList = ["seoul", "incheon", "busan", "gwangju", "jeju", "jeonju"]
+
+$(".temp").each(function(i) {
+    $(this).text(getCurrentTemp(cityList[i]) + "℃");
+});
+
+$(".location").on({
+    "click" : function() {
+        var q = $(this).children(".q").attr("id");
+        var redirectURL = "pages/weather_location.html?q=" + q; //경로는 항상 메인 페이지 기준
+        location.href = redirectURL;
+    }
+});
+
+
+
